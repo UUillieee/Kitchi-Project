1dbedd717113b3c656157ff5ce358d6943831838
JonghaPark <whdgk0754@gmail.com>
2025-09-08 03:17:48 +1200

    new: recipe detail without design

--- Kitchi-app/lib/spoonacular.tsx
+++ Kitchi-app/lib/spoonacular.tsx
@@ -1,7 +1,17 @@
 const apiKey= process.env.EXPO_PUBLIC_SPOONACULAR_API// take from .env file
 import { Recipe } from '../app/(tabs)/generateRecipes';
 
-
+export type recipeInfo ={
+    id: number;
+    title: string;
+    image: string; 
+    servings: number;
+    cookingMinutes: number;
+    summary: string;
+    instructions: string;
+    extendedIngredients: {original: string}[];
+    
+}
 
 export async function findRecipesByIngredients(userIngredients: string[], count: number): Promise<Recipe[]> {
   const ingredientStr = encodeURIComponent(userIngredients.join(','));
@@ -17,4 +27,28 @@ export async function findRecipesByIngredients(userIngredients: string[], count:
     console.error("Error fetching recipes:", error);
     return [];
   }
+}
+
+export async function getRecipeDetails(recipeId: number): Promise<recipeInfo | null> {
+  try {
+    const res = await fetch(`https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${apiKey}`);
+    const data = await res.json();
+    console.log("Recipe details data:", data);
+    return {
+      id: data.id,
+      title: data.title,
+      image: data.image,
+      servings: data.servings,
+      readyInMinutes: data.readyInMinutes,
+      summary: data.summary,
+      instructions: data.instructions,
+      extendedIngredients: data.extendedIngredients,
+    };
+    
+  } catch (error) {
+    console.error("Error fetching recipe details:", error);
+    return null;
+  }
+
+
 }
